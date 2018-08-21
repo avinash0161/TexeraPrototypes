8fee466ad216d84cd707dba9f7c8e63925737c25
Shengquan Ni <Hiseen@users.noreply.github.com>
2018-08-21 13:55:36 -0700

    update

--- TexeraOrleansPrototype/Program.cs
+++ TexeraOrleansPrototype/Program.cs
@@ -13,10 +13,12 @@ namespace TexeraOrleansPrototype
 {
     class Program
     {
-        private static int num_scan = 1;
+        private static int num_scan = 10;
         static async Task Main(string[] args)
         {
-            var siloBuilder = new SiloHostBuilder()
+            if (args[0] == "c")
+            {
+                var siloBuilder = new SiloHostBuilder()
                 .UseLocalhostClustering()
                 .AddSimpleMessageStreamProvider("SMSProvider")
                 // add storage to store list of subscriptions
@@ -30,11 +32,13 @@ namespace TexeraOrleansPrototype
                 .Configure<EndpointOptions>(options =>
                     options.AdvertisedIPAddress = IPAddress.Loopback)
                 .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Critical).AddConsole());
-
-            using (var host = siloBuilder.Build())
-            {
+                var host = siloBuilder.Build();
                 await host.StartAsync();
-
+                Console.WriteLine("Silo Started!");
+                Console.ReadLine();
+            }
+            else if (args[0] == "s")
+            {
                 var clientBuilder = new ClientBuilder()
                     .UseLocalhostClustering()
                     .AddSimpleMessageStreamProvider("SMSProvider")
@@ -44,115 +48,56 @@ namespace TexeraOrleansPrototype
                         options.ServiceId = "TexeraOrleansPrototype";
                     })
                     .ConfigureLogging(logging => logging.AddConsole());
+                var client = clientBuilder.Build();
+                await client.Connect();
+                Console.WriteLine("Server Started!");
+                Guid streamGuid = await client.GetGrain<ICountOperator>(1).GetStreamGuid();
 
-                using (var client = clientBuilder.Build())
+                Console.WriteLine("Client side guid is " + streamGuid);
+                var stream = client.GetStreamProvider("SMSProvider")
+                .GetStream<int>(streamGuid, "Random");
+                var so = new StreamObserver();
+                await stream.SubscribeAsync(so);
+
+                System.IO.StreamReader file = new System.IO.StreamReader(@"d:\small_input.csv");
+                int count = 0;
+                bool need_break = false;
+                List<IScanOperator> operators = new List<IScanOperator>();
+                for (int i = 0; i < num_scan; ++i)
                 {
-                    await client.Connect();
-
-                    Guid streamGuid = await client.GetGrain<ICountOperator>(1).GetStreamGuid();
-
-                    Console.WriteLine("Client side guid is " + streamGuid);
-                    var stream = client.GetStreamProvider("SMSProvider")
-                    .GetStream<int>(streamGuid, "Random");
-                    var so = new StreamObserver();
-                    await stream.SubscribeAsync(so);
-
-                    Task.Run(() => AcceptInputForPauseResume(client));
-
-                    System.IO.StreamReader file = new System.IO.StreamReader(@"d:\small_input.csv");
-                    int count = 0;
-                    bool need_break = false;
-                    List<IScanOperator> operators = new List<IScanOperator>();
+                    var t = client.GetGrain<IScanOperator>(i + 2);
+                    t.WakeUp();
+                    operators.Add(t);
+                    client.GetGrain<IFilterOperator>(i + 2).WakeUp();
+                    client.GetGrain<IKeywordSearchOperator>(i + 2).WakeUp();
+                    client.GetGrain<ICountOperator>(i + 2).WakeUp();
+                }
+                Thread.Sleep(1000);
+                await so.Start();
+                while (true)
+                {
+                    string line;
                     for (int i = 0; i < num_scan; ++i)
                     {
-                        var t = client.GetGrain<IScanOperator>(i + 2);
-                        t.WakeUp();
-                        operators.Add(t);
-                        client.GetGrain<IFilterOperator>(i + 2).WakeUp();
-                        client.GetGrain<IKeywordSearchOperator>(i + 2).WakeUp();
-                        client.GetGrain<ICountOperator>(i + 2).WakeUp();
-                    }
-                    Thread.Sleep(1000);
-                    await so.Start();
-                    while (true)
-                    {
-                        string line;
-                        for (int i = 0; i <num_scan; ++i)
-                        { 
-                            if ((line = file.ReadLine()) != null)
-                            {
-                                operators[i].SubmitTuples(new List<Tuple> { new Tuple(count, line.Split(",")) });
-                                count++;
-                            }
-                            else
-                                need_break = true;
-                        }
-
-                        if (need_break)
+                        if ((line = file.ReadLine()) != null)
                         {
-                            for (int i = 0; i < num_scan; ++i)
-                                operators[i].SubmitTuples(new List<Tuple> { new Tuple(-1, null) });
-                            break;
+                            operators[i].SubmitTuples(new List<Tuple> { new Tuple(count, line.Split(",")) });
+                            count++;
                         }
+                        else
+                            need_break = true;
                     }
-                    Console.WriteLine(count + "rows sent");
-                    Console.ReadLine();
-                    Console.WriteLine("Flushing the buffer and closing the filestreams...");
-                    for (int i = 0; i < num_scan; ++i)
-                        await client.GetGrain<IScanOperator>(i + 2).QuitOperator();
-                    Console.WriteLine("Complete!");
-                    Console.ReadLine();
-                    Console.WriteLine("Opening and merging...");
-                    Console.WriteLine("Report: Scan operator missed "+ReportMissing("Scan_",count)+" row(s)");
-                    Console.WriteLine("Report: Filter operator missed " + ReportMissing("Filter_", count) + " row(s)");
-                    Console.WriteLine("Report: Keyword operator missed " + ReportMissing("KeywordSearch_", count) + " row(s)");
-                    Console.WriteLine("Report: Count operator missed " + ReportMissing("Count_", count) + " row(s)");
-                    Console.WriteLine("Complete!");
-                    Console.ReadLine();
-                    
-                }
-            }
-        }
 
-        public static void AcceptInputForPauseResume(IClusterClient client)
-        {
-            while(true)
-            {
-                char input = Console.ReadKey().KeyChar;
-                if (input == 'p')
-                {
-                    // Console.WriteLine("Pause Called");
-                    for (int i = 0; i < num_scan; ++i)
-                        client.GetGrain<IScanOperator>(i + 2).PauseOperator();
+                    if (need_break)
+                    {
+                        for (int i = 0; i < num_scan; ++i)
+                            operators[i].SubmitTuples(new List<Tuple> { new Tuple(-1, null) });
+                        break;
+                    }
                 }
-                else if (input == 'r')
-                {
-                    // Console.WriteLine("Resume Called");
-                    for (int i = 0; i < num_scan; ++i)
-                        client.GetGrain<IScanOperator>(i + 2).ResumeOperator();
-                }
+                Console.WriteLine(count + "rows sent");
+                Console.ReadLine();
             }
-        }
-
-        public static int ReportMissing(string prefix,int count)
-        {
-            int res = 0;
-            bool[] l = new bool[count];
-            for (int i = 2; i < num_scan+2; ++i)
-            {
-                System.IO.StreamReader file = new System.IO.StreamReader(prefix+i.ToString());
-                string line;
-                while ((line = file.ReadLine()) != null)
-                {
-                    int temp = int.Parse(line);
-                    l[temp] = true;
-                }
-            }
-            foreach(var i in l)
-            {
-                if (!i) res++;
-            }
-            return res;
         }
     }
 }
