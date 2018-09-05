c54767663411d3c8dcc1baa50e2646e2928c7434
Shengquan Ni <Hiseen@users.noreply.github.com>
2018-09-05 16:09:07 -0700

    make it work

--- TexeraOrleansPrototype/ScanOperator.cs
+++ TexeraOrleansPrototype/ScanOperator.cs
@@ -19,7 +19,7 @@ namespace TexeraOrleansPrototype
         public override Task OnActivateAsync()
         {
             nextOperator = base.GrainFactory.GetGrain<IFilterOperator>(this.GetPrimaryKeyLong());
-            string p2 = @"d:\small_input_" + (this.GetPrimaryKeyLong() - 1) + ".csv";
+            string p2 = @"~/data/small_input_" + (this.GetPrimaryKeyLong() - 1) + ".csv";
             file = new System.IO.StreamReader(p2);
             return base.OnActivateAsync();
         }
