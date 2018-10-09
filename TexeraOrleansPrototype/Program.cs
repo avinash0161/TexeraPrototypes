2bc383b24cb9594ab156c6a09997cd6ab43be85f
Hiseen <hiseen@github.com>
2018-10-08 19:18:07 -0700

    test env update3

--- TexeraOrleansPrototype/Program.cs
+++ TexeraOrleansPrototype/Program.cs
@@ -17,7 +17,7 @@ namespace TexeraOrleansPrototype
         public const int num_scan = 1;
         public const bool conditions_on = false;
         public const bool ordered_on = false;
-        public const string dataset = "median"; // should be 'medium', but anyway...
+        public const string dataset = "large"; // should be 'medium', but anyway...
         public const string delivery = "Streams";
         public const string dir = @"/home/sheng/datasets/";
         static async Task Main(string[] args)
